486f2efcf0144ff50ef3572be970df9341c1099d
Prashant Sharma <prashant.sharma@tecxar.io>
2024-08-08 14:38:17 +0530

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import compression from 'compression';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(), { cors: true });
+  app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+
+  app.use(helmet());
+  // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
+  app.use(compression());
+  app.use(morgan('combined'));
+  app.enableVersioning();
+
+  // Starts listening for shutdown hooks
+  // if (!configService.isDevelopment) {
+  //   app.enableShutdownHooks();
+  // }
+
   await app.listen(3000);
 }
 bootstrap();
