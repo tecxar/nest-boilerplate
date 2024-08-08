3bab289bbd40d836fb4087264fabd8a7851658b2
Prashant Sharma <prashant.sharma@tecxar.io>
2024-08-08 21:08:01 +0530

    linting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,4 +5,8 @@ module.exports = {
   // ],
   // 'package.json': ['yarn lint:prettier'],
   // '*.md': ['yarn lint:markdownlint', 'yarn lint:prettier'],
+  '{src,apps,libs,test}/**/*.{ts,tsx}': ['eslint'],
+  '{src,apps,libs,test}/**/*.{js,jsx}': ['eslint'],
+  './**/*.{ts,tsx,js,jsx,json,rc,css,scss,less,html}': ['prettier --write'],
+  'package.json': ['npx -y sort-package-json'],
 };
