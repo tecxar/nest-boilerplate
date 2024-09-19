8b181aedd8bf06ff935741a6650afa5e4a048acd
Prashant Sharma <prashant.sharma@tecxar.io>
2024-09-19 23:30:13 +0530

    fix: lint-stage config fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,9 @@
 module.exports = {
 	// '*.ts': ['eslint --fix', 'git add'],
-	'{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['yarn format'],
-	'package.json': ['yarn format'],
-	'*.md': ['yarn lint:markdownlint', 'yarn format'],
-	'{src,apps,libs,test}/**/*.{ts,tsx}': ['eslint'],
-	'{src,apps,libs,test}/**/*.{js,jsx}': ['eslint'],
-	'./**/*.{ts,tsx,js,jsx,json,rc,css,scss,less,html}': ['prettier --write'],
-	'package.json': ['npx -y sort-package-json'],
+	// '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write'],
+	'package.json': ['npx -y sort-package-json', 'prettier --write'],
+	'*.md': ['yarn lint:markdownlint', 'prettier --write'],
+	'{src,apps,libs,test}/**/*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
+	'{src,apps,libs,test}/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
+	'./**/*.{ts,tsx,js,jsx,json,rc,css,scss,less,html}': ['eslint --fix', 'prettier --write'],
 };
