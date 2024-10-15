4fa510980b1427713a1b34407edf7ddc243d1e3f
bharat parmar <bharat.parmar@tecxar.io>
2024-10-15 18:19:30 +0530

    code setup

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// '*.ts': ['eslint --fix', 'git add'],
-	// '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write'],
-	'package.json': ['npx -y sort-package-json', 'prettier --write'],
-	'*.md': ['yarn lint:markdownlint', 'prettier --write'],
-	'{src,apps,libs,test}/**/*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
-	'{src,apps,libs,test}/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
-	'./**/*.{ts,tsx,js,jsx,json,rc,css,scss,less,html}': ['eslint --fix', 'prettier --write'],
-};
---
+++ sample/04-grpc/src/grpc-client.options.ts
@@ -0,0 +1,14 @@
+import { ReflectionService } from '@grpc/reflection';
+import { ClientOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+export const grpcClientOptions: ClientOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'hero', // ['hero', 'hero2']
+    protoPath: join(__dirname, './hero/hero.proto'), // ['./hero/hero.proto', './hero/hero2.proto']
+    onLoadPackageDefinition: (pkg, server) => {
+      new ReflectionService(pkg).addToServer(server);
+    },
+  },
+};
