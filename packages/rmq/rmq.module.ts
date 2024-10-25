d0dff5fa620fd40e3ba0ebb43b9976a4b89e7de1
bharat parmar <bharat.parmar@tecxar.io>
2024-10-25 13:44:26 +0530

    rmq module changes

--- packages/rmq/rmq.module.ts
+++ packages/rmq/rmq.module.ts
@@ -1,6 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RmqService } from './rmq.service';
+import { RmqModuleDefinition } from './rmq.module-definition';
 
 // Define the options for the RMQ module
 interface RmqModuleOptions {
@@ -12,9 +13,9 @@ interface RmqModuleOptions {
   providers: [RmqService],
   exports: [],
 })
-export class RmqModule {
+export class RmqModule extends RmqModuleDefinition.ConfigurableModuleClass {
   // Static method to register the RMQ module with dynamic options
-  static register({ name, rmqUri }: RmqModuleOptions): DynamicModule {
+  static forRoot({ name, rmqUri }: RmqModuleOptions): DynamicModule {
     return {
       module: RmqModule,
       imports: [
@@ -22,6 +23,7 @@ export class RmqModule {
           {
             name,
             useFactory: () => ({
+
               transport: Transport.RMQ,
               options: {
                 urls: [rmqUri || ''],
@@ -38,4 +40,40 @@ export class RmqModule {
       exports: [ClientsModule, RmqModule],
     };
   }
+
+  static forRootAsync(
+    options: typeof RmqModuleDefinition.ASYNC_OPTIONS_TYPE,
+  ): DynamicModule {
+    const module = super.forRootAsync(options);
+    return {
+      ...module,
+      imports: [
+        ...(options.imports || []),
+        ClientsModule.registerAsync([
+          {
+            name:'',
+            useFactory: async (...args) => {
+              const rmqOptions = await options.useFactory!(...args);
+  
+              return {
+                name:rmqOptions.name,
+                transport: Transport.RMQ,
+                options: {
+                  urls: [rmqOptions.rmqUri || ''],
+                  queue: `${name}_QUEUE`,
+                  queueOptions: {
+                    durable: true,
+                  },
+                  prefetchCount: 1,
+                },
+              }
+            },
+          }
+        ]),
+      ],
+      exports: [RedisMQ],
+      global: options.isGlobal,
+    };
+  }
+  
 }
