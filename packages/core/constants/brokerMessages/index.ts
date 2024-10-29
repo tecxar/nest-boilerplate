669a63b255432cae01e72a743f748ed4a3ad8dd9
cleardu-bhushan <bhushan@tecxar.io>
2024-10-29 16:33:49 +0530

    created schemas, interfaces of cdr and ezsip services

--- packages/core/constants/brokerMessages/index.ts
+++ packages/core/constants/brokerMessages/index.ts
@@ -1,3 +1,8 @@
+export const Services = {
+  CDR_SERVICE: 'CDR_SERVICE',
+  INTEGRATION_SERVICE: 'INTEGRATION_SERVICE',
+};
+
 export const BrokerMessages = {
   userMs: {
     user: {
@@ -5,8 +10,40 @@ export const BrokerMessages = {
     },
   },
   crm: {
-    clienet: {
+    client: {
       addClient: 'add_client',
+    },
+  },
+  cdr: {
+    processCdr: 'PROCESS-CDR',
+  },
+  integration: {
+    ezsip: {
+      login: 'LOGIN',
+      logout: 'LOGOUT',
+      agentReady: 'AGENT-READY',
+      agentBreak: 'AGENT-BREAK',
+      createAgent: 'CREATE-AGENT',
+      updateAgent: 'UPDATE-AGENT',
+      deleteAgent: 'DELETE-AGENT',
+      createExtension: 'CREATE-EXTENSION',
+      deleteExtension: 'DELETE-EXTENSION',
+      allAgent: 'ALL-AGENT',
+      createCampaign: 'CREATE-CAMPAIGN',
+      updateCampaign: 'UPDATE-CAMPAIGN',
+      CampaignById: 'CAMPAIGN-BY-ID',
+      deleteBulkLeads: 'DELETE-BULK-LEADS',
+      CampaignStartStop: 'CAMPAIGN-START-STOP',
+      allCampaigns: 'ALL-CAMPAIGN',
+      manualCall: 'MANUAL-CALL',
+      clickToCall: 'CLICK-TO-CALL',
+      endCall: 'END-CALL',
+      pauseCall: 'PAUSE-CALL',
+      listIVR: 'lIST-IVR',
+      agentLiveStatus: 'AGENT-LIVE-STATUS',
+      productivityReport: 'PRODUCTIVITY-REPORT',
+      activityReport: 'ACTIVITY-REPORT',
+      allBreak: 'ALL-BREAK',
     },
   },
 };
--- packages/core/schemas/cdr/index.ts
+++ packages/core/schemas/cdr/index.ts
@@ -4,113 +4,105 @@ import {
   DataType,
   AllowNull,
   Default,
+  AutoIncrement,
+  PrimaryKey,
 } from 'sequelize-typescript';
 import { ICdr } from '../../interfaces/cdr';
-import { Model } from 'sequelize';
+import { Model, Sequelize } from 'sequelize';
 import BaseModel from '../baseModel';
 
 @Table({ tableName: 'cdr' })
 export default class CdrSchema extends BaseModel<ICdr> implements ICdr {
+  @AutoIncrement
+  @PrimaryKey
+  @Column({ type: DataType.INTEGER.UNSIGNED })
+  public id: number;
+
   @AllowNull(false)
-  @Column(DataType.BIGINT.UNSIGNED)
-  number: number;
+  @Column(DataType.STRING(50))
+  public mobile: string;
 
   @Column(DataType.STRING(50))
-  userId: string;
+  public userId: string;
 
   @Column(DataType.INTEGER.UNSIGNED)
-  extension: number;
+  public extension: number;
 
   @AllowNull(true)
   @Column(DataType.STRING(25))
-  mode: string;
+  public mode: string;
 
   @AllowNull(true)
   @Column(DataType.STRING(255))
-  status: string;
+  public status: string;
 
   @Column(DataType.DATEONLY)
-  date: Date;
+  public date: string;
 
   @Column(DataType.INTEGER.UNSIGNED)
-  hour: number;
+  public hour: number;
 
   @Column(DataType.INTEGER.UNSIGNED)
-  minute: number;
+  public minute: number;
 
   @AllowNull(true)
-  @Column(DataType.STRING)
-  duration: string;
-
-  @AllowNull(true)
-  @Column(DataType.TIME)
-  queueDuration: string;
-
-  @AllowNull(true)
-  @Column(DataType.TIME)
-  ringDuration: string;
-
-  @AllowNull(true)
-  @Column(DataType.TIME)
-  wrapupTime: string;
-
-  @Column(DataType.STRING(20))
-  queueName: string;
-
   @Column(DataType.INTEGER.UNSIGNED)
-  listId: string;
+  public duration: number;
 
-  @Column(DataType.STRING(25))
-  did: string;
+  @AllowNull(true)
+  @Column(DataType.INTEGER.UNSIGNED)
+  public queueDuration: number;
+
+  @AllowNull(true)
+  @Column(DataType.INTEGER)
+  public ringDuration: number;
 
   @Column(DataType.STRING(50))
-  leadId: string;
+  public loanNumber: string;
 
   @Column(DataType.STRING(50))
-  loanNumber: string;
-
-  @Column(DataType.STRING(50))
-  referenceId: string;
+  public referenceId: string;
 
   @Column(DataType.STRING(40))
-  processName: string;
-
-  @Column(DataType.STRING(200))
-  recordingFileName: string;
+  public campaignName: string;
 
   @AllowNull(true)
   @Default(false)
   @Column(DataType.BOOLEAN)
-  isUnknown: boolean;
+  public isUnknown: boolean;
 
   @Column(DataType.INTEGER.UNSIGNED)
-  customerId: number;
+  public borrowerId: number;
 
   @AllowNull(true)
   @Column(DataType.TEXT)
-  customerName: string;
+  public borrowerName: string;
 
   @AllowNull(true)
   @Column(DataType.STRING)
-  startTime: string;
+  public startTime: string;
 
   @AllowNull(true)
   @Column(DataType.STRING)
-  endTime: string;
+  public endTime: string;
 
   @Default('Outbound')
   @Column(DataType.ENUM('Outbound', 'Inbound'))
-  direction: string;
+  public direction: string;
 
   @AllowNull(true)
-  @Column(DataType.STRING)
-  talkDuration: string;
+  @Column(DataType.INTEGER.UNSIGNED)
+  public talkDuration: number;
 
   @AllowNull(true)
-  @Column(DataType.TEXT)
-  comments: string;
+  @Column(DataType.INTEGER.UNSIGNED)
+  public waitDuration: number;
 
-  @AllowNull(true)
-  @Column(DataType.STRING)
-  waitDuration: string;
+  @Default(Sequelize.literal('CURRENT_TIMESTAMP'))
+  @Column(DataType.DATE)
+  public createdAt: Date;
+
+  @Default(Sequelize.literal('CURRENT_TIMESTAMP'))
+  @Column(DataType.DATE)
+  public updatedAt: Date;
 }
